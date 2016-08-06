522b3eb6e88bae9c3fe6890f76dfe310d80d2297
Narender Pal Singh <haanisingh@gmail.com>
2016-08-06 18:20:13 +0530

    Initial Commit

---
+++ Objective C Tutorial 2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Objective C Tutorial 2
+//
+//  Created by Narender Pal Singh on 06/08/16.
+//  Copyright © 2016 Narender Pal Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objective C Tutorial 2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Objective C Tutorial 2
+//
+//  Created by Narender Pal Singh on 06/08/16.
+//  Copyright © 2016 Narender Pal Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
